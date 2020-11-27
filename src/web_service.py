13aad73761b5ded9cdce95e00c1836aecb8f5e4c
Manuel Rodriguez <marg.2197@gmail.com>
2020-11-27 15:18:27 -0600

    Commit de proyecto de evaluación

---
+++ src/web_service.py
@@ -0,0 +1,21 @@
+import csv
+from flask import Flask, jsonify
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/', methods=['GET'])
+def read_csv():
+    file_path='../data/employees.csv'
+
+    list=[]
+
+    with open(file_path, 'r') as csv_file:
+        dic_reader=csv.DictReader(csv_file)
+        for dic in dic_reader:
+            list.append(dic)
+
+
+    return jsonify(list)
+
+if (__name__ == '__main__'):
+    app.run(debug=True, host='0.0.0.0')
