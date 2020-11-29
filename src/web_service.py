b7669bd269327c708f4575731f05338cfaefd81b
Manuel Rodriguez <marg.2197@gmail.com>
2020-11-29 17:13:02 -0600

    Agregar comentarios a código Manejo de excepciones

--- src/web_service.py
+++ src/web_service.py
@@ -5,17 +5,32 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/', methods=['GET'])
 def read_csv():
+
+    #Path to csv file
     file_path='data/employees.csv'
 
+    #List of employees
     list=[]
 
-    with open(file_path, 'r') as csv_file:
-        dic_reader=csv.DictReader(csv_file)
-        for dic in dic_reader:
-            list.append(dic)
+    try:
+        with open(file_path, 'r') as csv_file:
+
+            #Get employees in a dictionary
+            dic_reader=csv.DictReader(csv_file)
+
+            #Push each employee to list
+            for employee in dic_reader:
+                list.append(employee)
 
 
-    return jsonify(list)
+        return jsonify(list)
+
+    #Handle error
+    except EnvironmentError:
+        return({
+            'error': True,
+            'message': 'Oops! .csv file could not be found'
+        })
 
 if (__name__ == '__main__'):
     app.run(debug=True, host='0.0.0.0')
